144ca124923bf9eb1e7cc5705120c2e58ec1b37a
ashtneoi <ashtneoi@gmail.com>
2015-11-22 01:09:49 -0800

    Set address

--- firmware.s
+++ firmware.s
@@ -198,53 +198,6 @@ reset:      ; (state = ?)
 a0002:      nop
 a0003:      nop
 
-int:        btfss PIR2, 2 ; USBIF
-              return
-            btfsc UIR, 4 ; IDLEIF
-              *bra usbidle ; (Interrupts are disabled.)
-            btfsc UIR, 0 ; URSTIF
-              *bra usbreset ; (Interrupts are disabled.)
-            retfie
-
-usbidle:    ; Clear interrupt flags.
-            bcf UIR, 4 ; IDLEIF
-            bcf PIR2, 2 ; USBIF
-
-            ; Enable bus activity interrupt.
-            bsf UIE, 2 ; ACTVIE
-
-            ; Sleep until bus activity.
-            bcf LATC, 2
-            bsf UCON, 1 ; SUSPND
-            movlb UIR
-_ui_slp:    sleep
-            *btfss UIR, 2 ; ACTVIF
-              *bra _ui_slp ; (Interrupts are disabled.)
-
-            ; Disable bus activity interrupt.
-            bcf UIE, 2 ; ACTVIE
-
-            ; Wake up.
-            bcf UCON, 1 ; SUSPND
-
-            ; Clear bus activity flag.
-            movlb UIR
-_ui_actv:   *bcf UIR, 2 ; ACTVIF
-            *btfsc UIR, 2 ; ACTVIF
-              *bra _ui_actv ; (Interrupts are disabled.)
-
-            retfie
-
-usbreset:   ; Clear *all* interrupt flags.
-            clrf UIR
-            bcf PIR2, 2 ; USBIF
-
-            ; Reinitialize stack.
-            movlw 0x1F
-            movwf STKPTR ; (Interrupts are disabled.)
-
-            goto default
-
 
             ;;;
             ;;; main program
@@ -319,11 +272,6 @@ pllwait:    *btfss OSCSTAT, 6 ; PLLRDY
 
             ;;; Set up BD 0 (EP0 OUT). ;;;
 
-            ; UOWN = firmware, DTS = 0, DTSEN = on, BSTALL = off,
-            ; BC[9:8] = 0b00
-            movlw 0n00001000
-            movwf BD0STAT
-
             ; bank 2
             movlw 0x20
             movwf BD0ADRH
@@ -350,161 +298,84 @@ pllwait:    *btfss OSCSTAT, 6 ; PLLRDY
 
             ;;; Enable USB module. ;;;
 
-            movlw 0x50
-            movwf BD0CNT
-            movlw 0n10001000
-            movwf BD0STAT
-            bsf BD0STAT, 7 ; UOWN = SIE
-
-            movlw 0x50
-            movwf BD1CNT
-            movlw 0n10001000
-            movwf BD1STAT
-            bsf BD1STAT, 7 ; UOWN = SIE
-
             bsf UCON, 3 ; USBEN = on
 
             ;;; Wait for USB reset. ;;;
 
             movlb UIR
 rstwait:    *btfss UIR, 0 ; URSTIF
-              *bra usbreset ; (Interrupts are disabled.)
+              *bra rstwait ; (Interrupts are disabled.)
             bcf UIR, 0 ; URSTIF
 
-            ;;; Enable interrupts and endpoint 0. ;;;
+            movlb UCON
+            movlp se0wait
+se0wait:    *btfsc UCON, 5 ; SE0
+              *goto se0wait
+
+            ;;; Enable endpoint 0. ;;;
 
             bsf UEP0, 2 ; EPOUTEN
             bsf UEP0, 1 ; EPINEN
-
-            bsf PIE2, 2 ; USBIE
-            bsf INTCON, 6 ; PEIE
-            bsf INTCON, 7 ; GIE
 
             ;;; Handle setup transfers. ;;;
 
 default:    ; Wait for SETUP token.
 
-            ;movlb UCON
-            ;movlp _d_wait
-;_d_wait:    *btfsc UCON, 4 ; PKTDIS
-              ;*goto _d_wait
-
-            movlb BD0STAT
-            movlp _d_wait
-_d_wait:    *btfsc BD0STAT, 7 ; UOWN
-              *goto _d_wait
-
-            bsf LATC, 2
-
-            ;movf BD0STAT, 0
-            ;andlw 0n10000000 ; mask
-            ;sublw 0n10000000
-            ;movlb LATC
-            ;btfsc STATUS, 2 ; Z
-              ;*bsf LATC, 2
-
-            ;movf BD0CNT, 0
-            ;sublw 0x50
-            ;movlb LATC
-            ;btfsc STATUS, 2 ; Z
-              ;*bsf LATC, 2
-
-            ;call setup
-
             movlw 0x50
             movwf BD0CNT
-            movlw 0n10001000
+            movlw 0n00001000
             movwf BD0STAT
             bsf BD0STAT, 7 ; UOWN = SIE
 
+            movlb UCON
+            movlp _d_wait
+_d_wait:    *btfss UCON, 4 ; PKTDIS
+              *goto _d_wait
+
+            movlw 0
+            movwf BD1CNT
+            movlw 0n01001000
+            movwf BD1STAT
+            bsf BD1STAT, 7 ; UOWN = SIE
             bcf UCON, 4 ; PKTDIS
+            movlp _d_wait2
+_d_wait2:   *btfsc BD1STAT, 7 ; UOWN
+              *goto _d_wait2
+
+            movlw 0n00000100
+            xorwf LATC
 
             goto default
 
+            ;;;
+            ;;; control transfer, setup stage, SETUP transaction
+            ;;;
 
-setup:      ; If request type is Vendor or Reserved, ignore request.
-            movlp setup_done
+
+setup:      ; If request is too short, ignore it.
+            movf BD0CNT, 0
+            sublw 8
+            btfss STATUS, 2 ; Z
+              return
+
+            ; If request type is Vendor or Reserved, ignore request.
             btfsc bmRequestType, 6 ; Vendor or Reserved
-              *goto setup_done
+              return
 
-            movlp setup_cls
+            ;movlp setup_cls
             btfsc bmRequestType, 5 ; Class
-              *goto setup_cls
+              return
 
             movf bRequest, 0
-            movlp set_address
             sublw 5 ; SET_ADDRESS
+            movlp set_address
             btfsc STATUS, 2 ; Z
               *goto set_address
 
-            movf bRequest, 0
-            movlp get_descriptor
-            sublw 6 ; GET_DESCRIPTOR
-            btfsc STATUS, 2 ; Z
-              *goto get_descriptor
-
-            goto setup_done
-
-setup_done:
-            ;movlw 0n11111100 ; BC[9:8] mask
-            ;andwf BD1STAT
-            ;clrf BD1CNT
-            ;bsf BD1STAT, 7 ; UOWN
-            ;bcf UIR, 3 ; TRNIF
-
-            ;movlb BD1STAT
-            ;movlp _sd_uown
-;_sd_uown:   *btfsc BD1STAT, 7 ; UOWN
-              ;*goto _sd_uown
-
-            ;movlb UIR
-            ;movlp _sd_trn
-;_sd_trn:    *btfss UIR, 3 ; TRNIF
-              ;*goto _sd_trn
-            ;bcf UIR, 3 ; TRNIF
-
             return
+
 
 set_address:
             movf wValue, 0
             movwf UADDR
-            goto setup_done
-
-get_descriptor:
-            movlp _gd_eo
-            btfsc bmRequestType, 1
-              *goto _gd_eo
-
-            movlp _gd_i
-            btfsc bmRequestType, 0
-              *goto _gd_i
-
-            ;;; Get Device Descriptor ;;;
-
-            goto setup_done
-
-_gd_i:      ;;; Get Interface Descriptor ;;;
-
-            goto setup_done
-
-_gd_eo:     movlp _gd_o
-            btfsc bmRequestType, 0
-              *goto _gd_o
-
-            ;;; Get Endpoint Descriptor ;;;
-
-            goto setup_done
-
-_gd_o:      ;;; Get Other Descriptor (?) ;;;
-
-            goto setup_done
-
-
-
-
-setup_cls:  goto setup_done
-
-
-halt:       bcf UCON, 3 ; USBEN = off
-            movlp _h_loop
-_h_loop:    *goto _h_loop
+            return
