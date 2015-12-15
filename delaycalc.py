b0b47d5089b34aa4b83866715ce9828539748c64
ashtneoi <ashtneoi@gmail.com>
2015-12-15 11:16:38 -0800

    Fix delay calculator; generate code

--- delaycalc.py
+++ delaycalc.py
@@ -1,13 +1,58 @@
 #!/usr/bin/env python3
 
 
-from sys import argv
+from sys import argv, stderr, stdout
+
+
+outer_code = """\
+delay?_?:
+            movwf U7
+_7:{ins}\
+{w}\
+            decfsz U7
+              *bra _7
+            return
+"""
+
+
+middle_code = """\
+         movlw {ci}
+            movwf U{u}
+_{u}:{ins}\
+            decfsz U{u}
+              *bra _{u}
+"""
+
+
+inner_code = """\
+         movlw {c1}
+_w:\
+         decfsz WREG
+              *bra _w
+"""
+
+
+def gen_code(c, w):
+    cr = list(reversed(c))
+    def middle(c, u):
+        if len(c) > 1:
+            return middle_code.format(
+                ci=c[0],
+                u=u,
+                ins=middle(c[1:], u - 1),
+            )
+        else:
+            return inner_code.format(c1=c[0])
+
+    return outer_code.format(
+        ins=middle(c, 6),
+        w=("            nop\n" * w),
+    )
 
 
 def main(Fcy, Td, depth):
     Tcy = 1 / Fcy
     nT = Td / Tcy
-    print("Target cycle count = {}".format(nT))
     c = []
     c = [1] * depth
     bn = -1
@@ -19,7 +64,7 @@ def main(Fcy, Td, depth):
         for i in range(1, depth):
             n = (n + 3) * c[i] + 1
         n += 3
-        if bn < n <= nT:
+        if (bn < n <= nT):
             bn = n
             bc = c[:]
         while True:
@@ -33,8 +78,18 @@ def main(Fcy, Td, depth):
                 done = True
                 break
 
-    print(bn)
-    print(bc)
+    w = round(nT - bn)
+    if w > 10:
+        stderr.write("Error: {} nops is too many\n".format(w))
+        exit(1)
+
+    stdout.write((
+        "; target: {} cyc ({} s)\n"
+        "; actual: {} cyc ({} s)\n"
+        "; c = {}, w = {}\n"
+    ).format(nT, Td, round(nT), Tcy * round(nT), bc, w))
+    stdout.write(gen_code(bc, w))
+
 
 
 if __name__ == '__main__':
